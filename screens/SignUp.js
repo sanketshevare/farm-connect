7d001f1a5b7f9bda3196ba80c7ac759c73c7d0f4
sanketshevare <sanketshevare7675@gmail.com>
2023-03-12 14:25:39 +0530

    Solved

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -9,18 +9,5 @@ const SignUp = () => {
     </View>
   );
 };
+
 export default SignUp;
-import React from 'react'
-import { View, Text } from 'react-native'
-import tw from 'twrnc'
-
-const SignUp = () => {
-    return (
-        <View style={tw` bg-green-400 `}>
-            <Text>This is a Signup Screen</Text>
-        </View>
-    )
-}
-export default SignUp
-
-
