8a90bd46fad448f9a8111e45895bb06746ac3f4d
Shreyas002-6 <shreyas9822949536@gmail.com>
2023-03-12 13:50:38 +0530

    2nd commit

---
+++ screens/SignUp.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import tw from 'twrnc'
+
+const SignUp = () => {
+    return (
+        <View style={tw` bg-green-400 `}>
+            <Text>This is a Signup Screen</Text>
+        </View>
+    )
+}
+export default SignUp
+
+
