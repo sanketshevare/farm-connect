92dbec1eb6fe69b7a1252b00ebb9d52c8653834a
Shreyas002-6 <shreyas9822949536@gmail.com>
2023-03-16 00:28:20 +0530

    HomeFooter

--- screens/Home.js
+++ screens/Home.js
@@ -10,12 +10,31 @@ import News from "../footer/News";
 
 function Home() {
   const [selected, setSelected] = React.useState(1);
+  const [index, setIndex] = React.useState(0);
+function switchScreen(index) {
+    setIndex(index)
+    setSelected(index)
+ }
+let AppComponent = null;
+
+    if (index == 0) {
+       AppComponent = Posts
+    } 
+    else if(index == 1){
+       AppComponent = Blogs
+    }
+    else if(index == 2){
+      AppComponent = News
+   }
   return (
+    <>
+
+    <View><AppComponent/></View>
   <NativeBaseProvider>
       <View style = {tw`flex bg-white w-full`}>
         
         <HStack style = {tw`bg-green-400 items-center shadow-md`}>
-          <Pressable cursor="pointer" opacity={selected === 1 ? 1 : 0.6} py="2" flex={1} onPress={() => setSelected(1)}>
+          <Pressable cursor="pointer" opacity={selected === 0 ? 1 : 0.6} py="2" flex={1} onPress={() => switchScreen(0)}>
             <Center>
               <Icon mb="1" as={<Ionicons name={'home'} />} color="white" size="sm" />
               <Text color="white" fontSize="12">
@@ -23,7 +42,7 @@ function Home() {
               </Text>
             </Center>
           </Pressable>
-          <Pressable cursor="pointer" opacity={selected === 2 ? 1 : 0.6} py="2" flex={1} onPress={() => setSelected(2)}>
+          <Pressable cursor="pointer" opacity={selected === 1 ? 1 : 0.6} py="2" flex={1} onPress={() => switchScreen(1)}>
             <Center>
               <Icon mb="1" as={<Ionicons name={'globe-outline'} />} color="white" size="sm" />
               <Text color="white" fontSize="12">
@@ -31,7 +50,7 @@ function Home() {
               </Text>
             </Center>
           </Pressable>
-          <Pressable cursor="pointer" opacity={selected === 3 ? 1 : 0.5} py="2" flex={1} onPress={() => setSelected(3)}>
+          <Pressable cursor="pointer" opacity={selected === 2 ? 1 : 0.5} py="2" flex={1} onPress={() => switchScreen(2)}>
             <Center>
               <Icon mb="1" as={<Ionicons name={'newspaper-outline'} />} color="white" size="sm" />
               <Text color="white" fontSize="12">
@@ -42,6 +61,7 @@ function Home() {
         </HStack>
       </View>
     </NativeBaseProvider>
+    </>
     );
 }
 export default Home;
